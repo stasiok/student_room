bab0750bc76f3d24d50ae4c5f51cba168fae1ca6
Yatsenko Oleg <yatsenkoleg@ya.ru>
2015-06-18 19:56:36 +0300

    подсказки в форме авторизации

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?
 	include_once('sys/global.php');
-	//18,06,155555555
-	//1111111
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -17,7 +15,9 @@
 			<h1>student room</h1>
 			<h2>sumy state university</h2>
 			<input id="auth_rbn" type="text" placeholder="Номер зачетки"/>
+			<label for="auth_rbn">8-ми значный числовой код</label>
 			<input id="auth_pass" type="password" placeholder="Пароль" />
+			<label for="auth_pass">От 6 до 20 символов</label>
 			<div id="auth_send" class="button">Войти</div>
 		</div>
 	</body>
