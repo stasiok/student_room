6e876cfffc07cff2edbd7852dd31c3ddf0714f4f
Yatsenko Oleg <yatsenkoleg@ya.ru>
2015-06-18 19:51:14 +0300

    mysql_connect

--- sys/global.php
+++ sys/global.php
@@ -1,11 +1,32 @@
 <?
 	header('Content-Type:text/html; charset=UTF-8');
 	
-	$db=mysql_connect('127.0.0.1','root','');
+	/*mysql connect*/
+	$db_info=array(
+		"server"=>"mysql.hostinger.com.ua",
+		"login"=>"u546139275_data",
+		"pass"=>"z52565854Z",
+		"db"=>"u546139275_data",
+	);
+	if($_SERVER[COMPUTERNAME]=="SAMBER"){
+		$db_info=array(
+			"server"=>"127.0.0.1",
+			"login"=>"root",
+			"pass"=>"z52565854Z",
+			"db"=>"students_room",
+		);
+	}
+	
+	
+	$db=mysql_connect($db_info[server],$db_info[login],$db_info[pass]);
 	if(!$db)exit('db connect error');
 	
-	mysql_select_db('students_room',$db);
+	mysql_select_db($db_info[db],$db);
 	if(mysql_error())exit('select db error');
 	
+	mysql_set_charset('utf8',$db);
+	
 	define('DB',$db);
+	unset($db_info,$db);
+	/*mysql connect end*/
 ?>
